77c0b3769793c518ab5ada22832e768ad88bdb1c
isanfauji88 <isanfauji88@gmail.com>
2020-07-24 09:25:58 +0700

    Update module stock

--- Modules/Stock/Resources/views/itemcategory/show.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12 form-group">
-            <label for="name">Nama Kategori</label>
-            <div>{!! $model->name !!} </div>
-        </div>
-    </div>
-</div>
